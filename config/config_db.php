3705e0ef50163dea6e77e03405e3d630e64a1cd5
mariof16 <146434870+mariof16@users.noreply.github.com>
2023-12-15 09:40:57 +0100

    Update config_db.php

--- config/config_db.php
+++ config/config_db.php
@@ -1,9 +1,1 @@
-<?php
-define('HOST', '2daw.esvirgua.com');   // Dirección del servidor MySQL
-define('USER', 'user2daw_06');    // Nombre de usuario de MySQL
-define('PASSWORD', '{Nv2iujO@W@H'); // Contraseña de MySQL
-define('DATABASE', 'user2daw_BD2-06'); //Nombre de la base de datos
-define('CHARSET','utf8mb4');
 
-define('DEFAULT_CONTROLLER', 'instalacion');
-define('DEFAULT_ACTION', 'mostrarinstalacion');
